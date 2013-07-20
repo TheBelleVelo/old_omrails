cd49eb0480e011229ab89912591b1d6f170abe1b
Simon Masters <simonmasters@echelontek.com>
2013-07-21 08:35:47 +1000

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Omrails::Application.routes.draw do
+
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
-  # first created -> highest priority.
+  # first created -> highest priority.es
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
